403240eb57547762963b1cdae892405a96b45233
Nicholas <nicholasgswan@gmail.com>
2019-05-03 18:11:11 -0500

    first commit

---
+++ src/main/java/com/uglygrayduck/blog/BlogRefresherApplication.java
@@ -0,0 +1,13 @@
+package com.uglygrayduck.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogRefresherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogRefresherApplication.class, args);
+	}
+
+}
